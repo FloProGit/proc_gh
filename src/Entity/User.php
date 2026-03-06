88f57c3abe98181cfdff34873af027d3d11a813c
florian <sudreflorianpro@gmail.com>
2026-03-06 07:41:17 +0100

    fix(cs):break line remove

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,9 +64,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        if ($this->email === '') {
+        if ('' === $this->email) {
             throw new \LogicException('User email cannot be empty.');
         }
+
         return $this->email;
     }
 
