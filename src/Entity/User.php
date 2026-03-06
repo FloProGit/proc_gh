49c9ed11c9a9173b5ff1491e9e54c28d709a3307
florian <sudreflorianpro@gmail.com>
2026-03-06 07:31:26 +0100

    fix(entity): bad default value in user entity not null define and null by default

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,10 +17,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    private int $id = 0;
 
     #[ORM\Column(length: 180)]
-    private ?string $email = null;
+    private string $email = '';
 
     /**
      * @var list<string> The user roles
@@ -32,13 +32,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    private ?string $password = null;
+    private string $password = '';
 
     #[ORM\Column(length: 255)]
-    private ?string $firstName = null;
+    private string $firstName = '';
 
     #[ORM\Column(length: 255)]
-    private ?string $lastName = null;
+    private string $lastName = '';
 
     public function getId(): ?int
     {
